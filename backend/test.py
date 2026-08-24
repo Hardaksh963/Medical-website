a42fc892d21061d644e9031bc57cb06b881a2b13
Hardaksh963 <hardaksh369@gmail.com>
2026-08-24 23:20:56 +0530

    added the alembic tables and fixed the error that occured in the project

--- backend/test.py
+++ backend/test.py
@@ -1,15 +1,8 @@
 from sqlalchemy import create_engine, text
-
 from app.core.config import settings
-
 
 engine = create_engine(settings.DATABASE_URL)
 
-
 with engine.connect() as connection:
-
-    result = connection.execute(
-        text("SELECT current_database(), current_user")
-    )
-
+    result = connection.execute(text("SELECT version()"))
     print(result.fetchone())
